91f71228abf916e8e77c98f191a288058e44ab1c
cjy20 <2498525982@qqcom>
2018-06-10 00:17:37 +0800

    把远程的同步到本地

--- src/main/java/pos/controller/POSSalesController.java
+++ src/main/java/pos/controller/POSSalesController.java
@@ -277,23 +277,7 @@ public class POSSalesController {
 	} 	
 	}  	
 	
-	//做一个导出功能
-  @RequestMapping(value="/exportExcel",method={RequestMethod.POST,RequestMethod.GET}, produces="text/html,application/json;charset=UTF-8")
-  public void exportExcel(HttpServletRequest request,HttpServletResponse response){
-	  
-	  
-   try {
-		PrintWriter out=response.getWriter();
-		out.print("这个导出功能要求返回的数据");
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	  
-	  
-	  
-	  
-  }	  
+
 	
 	
 	
